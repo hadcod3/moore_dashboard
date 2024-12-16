824da45c86bac17f9570fc94aae7a0b682738f27
Had Code <decoding0506@gmail.com>
2024-12-16 14:20:41 +0700

    fixing auth clerk

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
     return (
         <div className="flex h-screen flex-col">
             <Aside/> 
-            <main className="flex-1 pl-20 pt-4 w-screen">{children}</main>
+            <main className="flex-1 pl-20 pt-4 w-full">{children}</main>
             <ToastContainer/>
         </div>
         
